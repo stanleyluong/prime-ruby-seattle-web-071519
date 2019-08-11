0e10a62fb9908bd0f0ee0a11341fe47229c5a0d3
github username <github email address>
2019-08-10 22:15:20 -0700

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,27 @@
 # Add  code here!
+def prime?(num)  
+    if num <= 1
+        return false
+    elsif num == 2
+        return true
+    elsif
+        for i in (2...num)
+            if num%i == 0
+                return false
+            end
+        end
+    end
+    true
+end
+
+# def prime?(integer)
+#     #prime number is only wholy divisible in itself and the integer one
+#       return true if integer == 2
+#       return false if integer < 1 || integer == 1
+#   for value in (2...integer)
+#       if integer % value == 0
+#         return false
+#       end
+#   end
+#   true
+# end 
